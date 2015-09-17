8fdcf60dfad06b628fe440e4d4463436c320846d
mmilutinovic1313 <mmilutinovic1313@gmail.com>
2015-09-16 21:13:57 -0400

    Added Desk stuff

---
+++ TurrificApp/src/turrificapp/Desk.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package turrificapp;
+
+import java.awt.Image;
+import java.awt.Rectangle;
+
+/**
+ *
+ * @author markymark1346
+ */
+public class Desk {
+    private int xCoordinate;
+    private int yCoordinate;
+    Image icon;
+    Rectangle bound;
+    private boolean objectPlaced;
+    
+}
