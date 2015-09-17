5714a55f7c9cb6b47777055d757c4a9424e44838
mmilutinovic1313 <mmilutinovic1313@gmail.com>
2015-09-16 20:58:45 -0400

    Added Board Class -- need to add properties

---
+++ TurrificApp/src/turrificapp/Board.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package turrificapp;
+
+/**
+ *
+ * @author markymark1346
+ */
+public class Board {
+    //TODO: We need Desk and Enemy before we can do Board
+}
