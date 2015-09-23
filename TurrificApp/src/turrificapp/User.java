03deb9671b0c73c49268c53871991286a4739cc0
mmilutinovic1313 <mmilutinovic1313@gmail.com>
2015-09-23 18:40:47 -0400

    Added User class

---
+++ TurrificApp/src/turrificapp/User.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package turrificapp;
+
+/**
+ *
+ * @author markymark1346
+ */
+public class User {
+    private String name;
+    private int score;
+    private int money;
+    private int health;
+  
+}
