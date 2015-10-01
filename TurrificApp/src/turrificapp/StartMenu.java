286c117cfd671741e9631f520d428fe33fe4d2e6
Brendan Kaulius <bkaulius@gmail.com>
2015-09-30 21:12:32 -0400

    StartMenu buttons as far as the eye can see

--- TurrificApp/src/turrificapp/StartMenu.java
+++ TurrificApp/src/turrificapp/StartMenu.java
@@ -7,6 +7,7 @@ package turrificapp;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.*;
+import java.awt.*;
 
 /**
  *
@@ -22,21 +23,50 @@ public class StartMenu extends JPanel implements ActionListener {
     private JLabel instructions;
 
     public StartMenu(){
+        this.setLayout(null);
         this.setSize(800, 600);
         
         easy = new JButton("Easy");
+        easy.setBounds(100, 500, 100, 50);
+        easy.setBackground(Color.white);
+        easy.setForeground(Color.black);
+        
         easy.addActionListener(this);
+        
         medium = new JButton("Medium");
+        medium.setBounds(100, 400, 100, 50);
+        medium.setBackground(Color.white);
+        medium.setForeground(Color.black);
         medium.addActionListener(this);
+        
         hard = new JButton("Hard");
+        hard.setBounds(100, 300, 100, 50);
+        hard.setBackground(Color.white);
+        hard.setForeground(Color.black);
         hard.addActionListener(this);
+        
         start = new JButton("Start");
+        start.setBounds(100, 200, 100, 50);
+        start.setBackground(Color.white);
+        start.setForeground(Color.black);
         start.addActionListener(this);
+        
+        nameInput = new JTextField("type in your name");
+        nameInput.setBounds(200, 500, 100, 50);
+        
+        highScore = new JLabel("high score");
+        highScore.setBounds(200, 400, 100, 50);
+        
+        instructions = new JLabel("Purchase and place turrets on the board using availabe funds and don't let enemies cross the endzone! Each wave will end when all the enemies have been destroyed or cross the endzone (accompanied with health loss). The game will terminate when your health reaches zero!");
+        instructions.setBounds(300, 500, 100, 300);
         
         this.add(easy);
         this.add(medium);
         this.add(hard);
         this.add(start);
+        this.add(nameInput);
+        this.add(highScore);
+        this.add(instructions);
         
         this.setVisible(true);
     }
