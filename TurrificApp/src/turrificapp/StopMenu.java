4e43a74d510baaffb892c20549d2ba7cc5ab163b
mmilutinovic1313 <mmilutinovic1313@gmail.com>
2015-09-30 20:34:13 -0400

    Added some more stuffs

--- TurrificApp/src/turrificapp/StopMenu.java
+++ TurrificApp/src/turrificapp/StopMenu.java
@@ -5,6 +5,7 @@
 package turrificapp;
 
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.*;
 
 /**
@@ -20,12 +21,34 @@ public class StopMenu extends JPanel implements ActionListener  {
         super();
         quit = new JButton("Quit");
         mainMenu = new JButton("Main Menu");
+        resume = new JButton("resume");
+        
         this.add(quit);
         this.add(mainMenu);
         this.add(resume);
     }
     
-    public void goToMenu(){
+    public void goToMenu(){ // This is literally actionPerformed
         //creates startMenu
+        
     }
+
+    public void actionPerformed(ActionEvent evt) {
+        Object obj = evt.getSource();
+        
+        if(obj == quit){
+            System.exit(0);
+        }
+        if(obj == mainMenu){
+            // go back to the panel that does that main menu stuff
+            StartMenu theMenu = new StartMenu();
+            this.remove(this);
+        }
+        if(obj == resume){
+            // remove the frame 
+            this.remove(this);
+        }
+    }
+    
+    
 }
